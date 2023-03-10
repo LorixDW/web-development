fba84cb8f0b65c8a565093362ba8d8f3bc517293
Lorix <pizhyanov.slava@yandex.ru>
2023-03-10 15:08:35 +0300

    app.ts

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,4 +1,12 @@
 function sum(a:number, b:number){
     return a + b;
 }
+interface IceCream{
+    flavor: string;
+    scoope: number;
+}
+let myIceCream: IceCream = {
+    flavor: "beakon",
+    scoope: 5
+}
 console.log(sum(3, 4));
